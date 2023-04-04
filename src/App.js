397b107d964ef867fe4f7d5e587df67ca3339f42
watorutart <46208434+watorutart@users.noreply.github.com>
2023-04-04 21:47:55 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [todoText, setTodoText] = useState("");
+  const [incompleteTodos, setIncompleteTodos] = useState(["あああ", "いいい"]);
+  const [completeTodos, setCompleteTodos] = useState(["ううう"]);
+
+  const onChangeText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {};
+
+  return (
+    <>
+      <div className="inputTodo">
+        <input
+          placeholder="ToDoを入力"
+          value={todoText}
+          onChange={onChangeText}
+        />
+        <button onClick={onClickAdd}>追加</button>
+      </div>
+      <div className="incompleteList">
+        <p className="title">未完了タスク</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="completeList">
+        <p className="title">完了タスク</p>
+        <ul>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+}
